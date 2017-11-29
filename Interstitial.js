a838e064570ce564b74a8854a1cfe3d3fe78819c
ADSNATA <30455946+adsnata@users.noreply.github.com>
2017-11-29 11:00:16 +0700

    Add files via upload

---
+++ Interstitial.js
@@ -0,0 +1,26 @@
+<code><script type="text/javascript">
+ var Appnext = {
+ android_id: 'xx',
+ ios_id: 'xx',
+ cat: 'xx',
+ pbk: 'xx',
+ b_title: 'xx',
+ b_color: 'xx',
+ auto_play: 'xx',
+ timeout: 'xx',
+ times_to_show: 'xx',
+ times_to_show_reset: 'xx',
+ creative:'managed',
+ subid: 'xxx',
+ ad_server:'false'
+ };
+ (function(){
+ var _s=document.createElement('script');
+ _s.type='text/javascript';
+ _s.async=true;
+ _s.src=
+'https://adsnata.com/ads/manage_script.js' ;
+ (document.body)?document.body.appendChild(_s):document.head.
+appendChild(_s);
+ })();
+</script></code>
